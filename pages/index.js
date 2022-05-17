d4da5b657e196d54f1141dfe6ef6b2e9268aa6a4
kmckelvey2020 <103072346+kmckelvey2020@users.noreply.github.com>
2022-05-16 20:46:19 -0400

    Updating  node dependencies

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,7 @@ import Image from "next/image";
 import styles from "./index.module.css";
 
 export default function LandingPage() {
-  const { Client } = require('pg');
-
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-
-  client.connect();
-
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
-
+  
   return (
     <div className={ styles.landing_container }>
       <Head>
